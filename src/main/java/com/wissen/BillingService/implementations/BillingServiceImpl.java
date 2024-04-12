d77f11fb85bfa20e58fe732b4e3d0d3967b7ad85
PrashantRJoshi <prashant.joshi@wissen.com>
2024-04-12 13:21:39 +0530

    Updated Controllers

--- src/main/java/com/wissen/BillingService/implementations/BillingServiceImpl.java
+++ src/main/java/com/wissen/BillingService/implementations/BillingServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BillingServiceImpl implements BillingService {
@@ -54,6 +55,11 @@ public class BillingServiceImpl implements BillingService {
     }
 
     @Override
+    public List<Billing> getBillsByMeterIdAndPaymentStatus(long meterId, PayStatus status){
+        return repository.findByPaymentStatus(PayStatus.UNPAID).stream().filter((bill) -> bill.getMeterId() == meterId).collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
     public List<Billing> getBillsByPaymentStatus(PayStatus status) {
         return repository.findByPaymentStatus(status);
     }
@@ -64,12 +70,12 @@ public class BillingServiceImpl implements BillingService {
     }
 
     @Override
-    public List<Billing> getBillsByMonth(String month) {
-        return null;
+    public Billing getBillsByMonth(long meterId, int month) {
+        return getBillByMonthAndYear(meterId, month, LocalDate.now().getYear());
     }
 
     @Override
-    public Billing getBillByMonthAndYear(Long meterId, int month, int year) {
+    public Billing getBillByMonthAndYear(long meterId, int month, int year) {
         return repository.getBillByMonthAndYear(meterId, month, year).orElse(null);
     }
 
